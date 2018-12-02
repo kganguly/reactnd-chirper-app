038c3372c7b53a29caa607953124ae2a7fb20ae3
Kaushik Ganguly <kganguly@gmail.com>
2018-12-02 12:51:59 -0500

    - Add toggle tweet reducer.

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,13 +1,23 @@
-import { RECEIVE_TWEETS } from '../actions/tweets';
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets';
 
 export default function tweets(state = {}, action) {
   switch (action.type) {
-    case RECEIVE_TWEETS:
-      return {
-        ...state,
-        ...action.tweets
-      };
-    default:
-      return state;
+  case RECEIVE_TWEETS:
+    return {
+      ...state,
+      ...action.tweets
+    };
+  case TOGGLE_TWEET:
+    return {
+      ...state,
+      [action.id]: {
+        ...state[action.id],
+        liked: action.hasLiked === true
+          ? state[action.id].likes.filter(uid => uid !== action.authedUser)
+          : state[action.id].likes.concat([action.authedUser])
+      }
+    };
+  default:
+    return state;
   }
 }
